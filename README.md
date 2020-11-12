f0e8ac082967dde6fbf9c19a517f72e8dea6958d
DaviLimaPereira <davilimapereira@hotmail.com>
2020-11-12 23:33:32 +0000

    Inicio do Projeto

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # TrabalhoPraticoTopicosEspeciaisEmInformatica
 Este repositório contém o Trabalho Prático da disciplina de Tópicos Especiais em Informática
+
+Empresa Metalurgica
+
+- RH (treinamentos, currículo do funcionário, benefícios, demostrativo pagamento)
+
+- Manutenção (solicitação, execução, materiais usados, mão-de-obra usada, consolidação mensal) 
+
+- Suprimentos (materiais, controle dos estoque mínimo, fornecedores, pedido de compra, recebimento de materiais) 
+
+- Vendas (produtos, clientes, notas fiscais de vendas)
+
+- Chão de fábrica: (máquinas, produção diária de produtos, estoque de produtos)
+
+Este projeto irá ser responsável pelo seguinte componente:
+
+Manter cadastro de benefícios (valor de desconto conforme faixa salarial). Demonstrativo pagamento: salário menos descontos benefícios.
+
+Grupo:
+Davi Lima Pereira
+
+O projeto deve obedecer os seguintes requisitos não funcionais:
+
+1) Laravel
+2) Bootstrap
+3) Paginação onde couber
+4) Funcionalidades de filtros e pesquisas
+5) Validação dos dados de formulários usando Form Request
+6) Autenticação de usuários padrão do Laravel
+7) Upload e apresentação de imagens
---
+++ TpGrupo2/app/Http/Controllers/BeneficiosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Beneficios;
+use Illuminate\Http\Request;
+
+class BeneficiosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Beneficios  $beneficios
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Beneficios $beneficios)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Beneficios  $beneficios
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Beneficios $beneficios)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Beneficios  $beneficios
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Beneficios $beneficios)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Beneficios  $beneficios
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Beneficios $beneficios)
+    {
+        //
+    }
+}
